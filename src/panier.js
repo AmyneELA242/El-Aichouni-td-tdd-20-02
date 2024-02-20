4267095be76cdf8de0cbfef4e1347e49f8527808
AmyneELA242 <amyne.elaichouni@gmail.com>
2024-02-20 19:55:32 +0100

    Commit [ADD] Etape 3-Ajout de code sup

--- src/panier.js
+++ src/panier.js
@@ -9,15 +9,25 @@ class Panier {
     }
 
     appliquerCoupon(coupon) {
-        this.articles.forEach(article => {
-            if (article.prixRemise === article.prix && coupon.pourcentageRemise > 0) {
-                const remise = article.prix * (coupon.pourcentageRemise / 100);
-                article.prixRemise = Math.max(article.prix - remise, 0);
-                this.remiseTotale += remise;
-            }
-        });
-
+        if (this.coupons.includes(coupon)) {
+            this.articles.forEach(article => {
+                if (article.prixRemise === article.prix && coupon.pourcentageRemise > 0) {
+                    const remise = article.prix * (coupon.pourcentageRemise / 100);
+                    const nouveauPrixRemise = Math.max(article.prix - remise, 0);
+    
+                    if (nouveauPrixRemise < article.prixRemise) {
+                        console.log(`La remise de ${coupon.pourcentageRemise}% provoquerait un prix négatif pour ${article.nom}.`);
+                    } else {
+                        article.prixRemise = nouveauPrixRemise;
+                        this.remiseTotale += remise;
+                    }
+                }
+            });
+        } else {
+            console.log(`Le coupon '${coupon.code}' n'existe pas.`);
+        }
     }
+
     calculerMontantTotal() {
         return this.articles.reduce((total, article) => total + article.prixRemise, 0) - this.remiseTotale;
     }
